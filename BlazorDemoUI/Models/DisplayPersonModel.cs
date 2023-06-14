7a4200c5de45e0b6796529e99066740daa8027b0
Jefffer <jefre123@hotmail.com>
2023-06-14 14:22:57 +0200

    Insert logic added to blazor page

---
+++ BlazorDemoUI/Models/DisplayPersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoUI.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First Name os too long.")]
+        [MinLength(5, ErrorMessage = "First Name is to short.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last Name os too long.")]
+        [MinLength(5, ErrorMessage = "Last Name is to short.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
