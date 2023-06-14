39be639ac860473a711e2fea10949fec2b15f97a
Jefffer <jefre123@hotmail.com>
2023-06-14 11:24:47 +0200

    Added DataAccessLibrary for Dapper use

---
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
